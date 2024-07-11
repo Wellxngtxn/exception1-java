73b4ac00f70b54007f12215e2ebfd20210ea0003
Wellington <wellingtoncerqueira17@gmail.com>
2024-07-11 16:30:02 -0300

    Bad Solution

--- src/Model/entities/Reservation.java
+++ src/Model/entities/Reservation.java
@@ -1,4 +1,3 @@
-
 package Model.entities;
 
 import java.text.SimpleDateFormat;
@@ -6,11 +5,11 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class Reservation {
-    
+
     private Integer roomNumber;
     private Date checkIn;
     private Date checkOut;
-    
+
     private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
     public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
@@ -34,15 +33,26 @@ public class Reservation {
     public Date getCheckOut() {
         return checkOut;
     }
-    
-    public long duration(){
+
+    public long duration() {
         long diff = checkOut.getTime() - checkIn.getTime();
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
-    
-    public void updateDates(Date checkIn, Date checkOut){
+
+    public String updateDates(Date checkIn, Date checkOut) {
+
+        Date now = new Date();
+        if (checkIn.before(now) || checkOut.before(now)) {
+            return "Error in reservation: Reservation dates for update must be future dates";
+        }
+        if (!checkOut.after(checkIn)) {
+            return "Error in reservation: Check-out date must be after check-in date";
+        }
+        
         this.checkIn = checkIn;
         this.checkOut = checkOut;
+        
+        return null;
     }
 
     @Override
